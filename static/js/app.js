a620ad5884cc965b588ba11f01cbfc07b0142485
adinger1 <anikadinger512@gmail.com>
2023-05-29 17:56:50 -0400

    5:56 update

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,16 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
+
+
 d3.json(url).then(function(data){
     console.log(data);
 });
 
 let ids = Object.values(data.names)
-console.log(ids);
+    console.log(ids);
 
 let sampleValues = data.sort((a,b) => b.samples.sampleValues - a.samples.sampleValues)
 console.log(sampleValues)
@@ -17,8 +22,8 @@ reversedData = sampleValues.reverse()
 // Display the default plot
 function init() {
     let data = [{
-      values: australia,
-      labels: labels,
+      values: sample_values,
+      labels: otu_labels,
       type: "bar",
       orientation:'h'
     }];
