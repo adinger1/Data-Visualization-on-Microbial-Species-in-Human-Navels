3bc37739bbaf3f02ebed5cc26cfc100b0481e08e
adinger1 <anikadinger512@gmail.com>
2023-06-01 17:33:59 -0400

    Bar graph finished

--- static/js/app.js
+++ static/js/app.js
@@ -1,74 +1,115 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-d3.json(url).then(function(data){
-    console.log(data);
-
-    //initializeBar(data);
-    getData(data);
-});
-
-let ids = Object.values(data.names)
-    console.log(ids);
-
-let sampleValues = data.sort((a,b) => b.samples.sampleValues - a.samples.sampleValues)
-console.log(sampleValues)
-// Slice top 10 OTUs for plotting
-slicedData = sampleValues.slice(0,10)
-// Reverse data to accommodate Plotly's defaults
-reversedData = sampleValues.reverse()
+let initialized = false
+// console.log(sampleValues)
+// // Slice top 10 OTUs for plotting
+// slicedData = sampleValues.slice(0,10)
+// // Reverse data to accommodate Plotly's defaults
+// reversedData = sampleValues.reverse()
 
 // Display the default plot
-function init() {
-    let data = [{
-      values: sample_values,
-      labels: otu_labels,
-      type: "bar",
-      orientation:'h'
-    }];
+
+function updateBar(data,subject_id){
+  console.log(`updateBar ${subject_id}`);
+  //get otu_ids
+  let id_dictionary = findDictionary(data["samples"],'id',subject_id)
+
+  //values for bar graph
+  sampleValues = id_dictionary["sample_values"]
+  //sampleValuesReversed = sampleValues.reverse()
+  top_10_sampleValues = sampleValues.slice(0,10)
+  //Reversing to graph in descending order
+  reversed_top10Samples = top_10_sampleValues.reverse()
   
-    let layout = {
-      height: 600,
-      width: 800
-    };
+  console.log(top_10_sampleValues)
+
+  //labels for bar graph
+  otu_ids_array = id_dictionary["otu_ids"]
+  //otu_ids_reversed = otu_ids_array.reverse()
+  top_10_otuIDs = otu_ids_array.slice(0,10)
+  //Reversing to graph in descending order
+  reversed_otuIDs = top_10_otuIDs.reverse()
+
+  yaxislabels = []
+  for(i=0; i< reversed_otuIDs.length; i++){
+    yaxislabels.push("OTU " + reversed_otuIDs[i])
+  }
+
+
+  console.log(top_10_otuIDs)
+  console.log(yaxislabels)
+  //hovertext for bar graph
+  otu_labels = id_dictionary["otu_labels"].slice(0,10)
+  console.log(otu_labels)
+
   
-    Plotly.newPlot("plot", data, layout);
-}
-
-function getData(){
-    let dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a letiable
-    let dataset = dropdownMenu.property("value");
-    // Initialize an empty array for the country's data
-    let plot_data = [];
-
-    if(names.includes(dataset)){
-      //plot_data = data for each ID
-    }
-
-    updatePlotly(data);
-
-}
-let trace1 = {
-    x: reversedData.map(object => object.sample_values),
-    y: reversedData.map(object => object.otu_ids),
-    text: reversedData.map(object => object.otu_labels),
-    type:"bar",
+  let bar_data = [{
+    x: reversed_top10Samples,
+    y: yaxislabels,
+    text: yaxislabels,
+    type: "bar",
     orientation:"h"
-};
+  }]
 
-let traceData = [trace1]
 
-let layout = {
-    title: "Top 10 OTU IDs",
-    margin: {
-      l: 100,
-      r: 100,
-      t: 100,
-      b: 100
+  let layout = {
+    title: "Top 10 OTU IDs for Subject " + subject_id
+  }
+
+  if(!initialized){
+    Plotly.newPlot("bar",bar_data,layout);
+    let initialized = true
+  }
+  else{
+    updatePlotly(bar_data)
+  }
+  
+}
+
+function findDictionary(arr,key,value){
+  for(i=0; i < arr.length; i++){
+    if(arr[i][key] === value){
+      return arr[i]
     }
-  };
+  }
+  return null
+}
 
-Plotly.newPlot("plot",traceData,layout)
+function updateBubble(data,subject_id){
+
+  console.log(`updateBubble ${subject_id}`);
+}
+
+
+function updateDemo(data,subject_id){
+  console.log(`updateDemo ${subject_id}`);
+  console.log(data);
+}
+
+
+function optionChanged(subject_id){
+  d3.json(url).then(function(data){
+    updateBar(data, subject_id);
+    updateBubble(data, subject_id);
+    updateDemo(data, subject_id);
+  });
+}
+
+
+function init() {
+  let dropdownMenu = d3.select("#selDataset");
+  d3.json(url).then(function(data){
+    subject_ids = data.names;
+
+    // dropdownMenu.append("option").attr("value", "940").text("940");
+    for(i = 0; i < subject_ids.length; i++){
+      dropdownMenu.append("option").attr("value", subject_ids[i]).text(subject_ids[i]);
+    }
+    optionChanged(subject_ids[0]);
+  });
+}   
+
+// Plotly.newPlot("plot",traceData,layout)
 function updatePlotly(newdata) {
   Plotly.restyle("bar", "values", [newdata]);
 }
